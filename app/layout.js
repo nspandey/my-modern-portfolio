65792464b08f15a0fbb622671eac4ddf8407768e
nspandey <nspandey1991@gmail.com>
2025-02-20 18:27:49 +0530

    Updated PortFolio

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en' className='scroll-smooth'>
       <body
-        className={`${outfit.className} ${ovo.className} antialiased leading-none overflow-x-hidden dark:bg-darkTheme dark:text-white`}
+        className={`${outfit.className} ${ovo.className} antialiased leading-tight overflow-x-hidden dark:bg-darkTheme dark:text-white`}
       >
         {children}
       </body>
