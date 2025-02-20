2bbd531256edb52e4ba4451d01ad781cf06f48bc
nspandey <nspandey1991@gmail.com>
2025-02-20 18:22:27 +0530

    Updated PortFolio

--- components/About.jsx
+++ components/About.jsx
@@ -6,7 +6,7 @@ const About = ({ isDarkMode }) => {
   return (
     <motion.div
       id='about'
-      className='w-full px-[12%] py-10 scroll-mt-20'
+      className='w-full px-[12%] mt-20 py-10 scroll-mt-20'
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1 }}
